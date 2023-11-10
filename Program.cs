6b42b5fc3f927b6f374e6b1dc3eaa06b0b36f8c7
Robervan Souza <103292038+RobervanSouza@users.noreply.github.com>
2023-11-10 12:35:41 -0300

    Criando class

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("ola mundo"); // teste
+﻿//Apicativo musica
+
+ Musica musica1 = new Musica();
+Console.WriteLine("****************************************************");
+Console.WriteLine("Artista 01");
+
+musica1.nome = "primeira musica";
+musica1.duracao = 112;
+musica1.artista = "artista1"; 
+musica1.disponivel = true;
+musica1.ExibirFichaMusicas();
+
+Musica musica2 = new Musica();
+Console.WriteLine("\n****************************************************");
+Console.WriteLine("Artista 02");
+musica2.nome = "musica2";
+musica2.artista = "artista2";
+musica2.disponivel = false;
+musica2.duracao = 112;
+musica2.ExibirFichaMusicas();
---
+++ musicas.cs
@@ -0,0 +1,27 @@
+﻿
+     class Musica
+    {
+       public  string nome;
+       public string artista;
+       public int duracao;
+       public bool disponivel;
+
+     public void ExibirFichaMusicas() 
+     {
+        Console.WriteLine($"Nome: {nome} ");
+        Console.WriteLine($"Artista: {artista} ");
+        Console.WriteLine($"Duração: {duracao} ");
+
+        if (disponivel)
+        {
+            Console.WriteLine("Musica Disponivel no plano plus");
+
+        }
+        else
+        {
+            Console.WriteLine("Não disponivel no plano, Adquira um plano plus");
+        }
+    
+     } 
+}
+
